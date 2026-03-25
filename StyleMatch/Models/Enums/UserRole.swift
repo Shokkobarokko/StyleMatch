51bdf4e7928490e9146c18b08b16751d0d874785
Avetis <avetis.01@mail.ru>
2026-03-25 22:38:41 +0300

    Определяем роль пользователя

---
+++ StyleMatch/Models/Enums/UserRole.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+enum UserRole: String, Codable {
+    case client = "client"
+    case stylist = "stylist"
+    
+    var displayName: String {
+        switch self {
+        case .client: return "Клиент"
+        case .stylist: return "Стилист"
+        }
+    }
+}
